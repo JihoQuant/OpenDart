d67eb0a2c366f5a0111d0494c3eef227cb1ad8ce
Jihogrammer <jiho@kakao.com>
2022-05-20 23:05:30 +0900

    ⭐️ deploy 0.0.3
    - 개발용 배포용 venv 구분
    - import path 수정
    - 문서 개선
    - pypi classifiers 추가

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name                          = 'OpenDart',
-    version                       = '0.0.2',
+    version                       = '0.0.3',
     description                   = '전자공시 OPEN DART 시스템 API',
     long_description              = open('README.md').read(),
     long_description_content_type = 'text/markdown',
@@ -12,7 +12,12 @@ setup(
     packages                      = find_packages(),
     install_requires              = ['python-dotenv', 'requests', 'xmltodict'],
     classifiers                   = [
+        # https://pypi.org/classifiers
+        "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: Korean",
+        "Environment :: Web Environment",
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
+        "Topic :: Office/Business",
     ],
 )
