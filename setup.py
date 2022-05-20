6fce086d9c08182c3636631e8ed7e1202b061e11
Jihogrammer <jiho@kakao.com>
2022-05-20 16:59:06 +0900

    ⚙️ config environments

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                          = 'OpenDart',
+    version                       = '0.0.1',
+    description                   = '전자공시 OPEN DART 시스템 API',
+    long_description              = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    author                        = 'jihogrammer',
+    author_email                  = 'jihogrammer@gmail.com',
+    license                       = 'MIT',
+    packages                      = find_packages(),
+    install_requires              = ['python-dotenv', 'requests'],
+    classifiers                   = [
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
