34fdad921cb45087f3f1c7698c79ddf35314fe34
Jihogrammer <jiho@kakao.com>
2022-05-27 08:52:25 +0900

    ⭐️ deploy 0.1.0
    - aiohttp 비동기 통신 적용
    - 프로젝트 개선

--- aio_test.py
+++ aio_test.py
@@ -1,15 +1,21 @@
 import asyncio
 
-from opendart.async_apis.public_notice import fetch_all_listed_corp
-from opendart.async_apis.business_report import fetch_total_issued_stock_by_corp_code
+from opendart.utils import save_json_as_file
+from opendart.async_apis import fetch_all_listed_corp, fetch_total_issued_stock_by_corp_code
 
 async def print_all_issued_stocks():
     corps = await fetch_all_listed_corp()
     result = await asyncio.gather(
         *[fetch_total_issued_stock_by_corp_code(corp['corp_code']) for corp in corps]
     )
-    for i in range(len(corps)):
-        if result[i]:
-            print(corps[i]['corp_name'], result[i])
+    for i, corp in enumerate(corps):
+        corp['issued_stock'] = result[i]
+        if corp['issued_stock']:
+            print(corp)
+    return corps
 
-asyncio.run(print_all_issued_stocks())
+save_json_as_file(asyncio.run(print_all_issued_stocks()), 'issued')
+
+
+from datetime import date
+int(''.join(date.today().isoformat().split('-')))
